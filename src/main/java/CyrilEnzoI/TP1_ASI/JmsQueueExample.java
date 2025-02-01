5b84ed926261e918a2321da75d5c22f54050e5fa
leyzow <enzo.ihadjadene@efrei.net>
2025-02-01 19:02:25 +0100

    JmsQueueExample finito

--- src/main/java/CyrilEnzoI/TP1_ASI/JmsQueueExample.java
+++ src/main/java/CyrilEnzoI/TP1_ASI/JmsQueueExample.java
@@ -1,65 +1,42 @@
 package CyrilEnzoI.TP1_ASI;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
+import javax.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerFactory;
-import org.apache.activemq.broker.BrokerService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JmsQueueExample {
-    public static void main(String[] args) throws URISyntaxException, Exception {
-        // Étape 1 : Configurer et démarrer le broker ActiveMQ
-        BrokerService broker = BrokerFactory.createBroker(new URI("broker:(tcp://localhost:61616)"));
-        broker.start(); // Démarrer le broker
+
+    private final ConnectionFactory connectionFactory;
+
+    public JmsQueueExample() {
+        this.connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+    }
+
+    public void processQueue() throws Exception {
         Connection connection = null;
-
+        Session session = null;
         try {
-            // Étape 2 : Créer une connexion au broker ActiveMQ
-            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
             connection = connectionFactory.createConnection();
-
-            // Étape 3 : Créer une session JMS
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            // Étape 4 : Créer une file (Queue)
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Queue queue = session.createQueue("customerQueue");
 
-            // Étape 5 : Créer un message à envoyer
+            // Envoi du message
+            MessageProducer producer = session.createProducer(queue);
             String payload = "Message important à traiter";
             Message message = session.createTextMessage(payload);
-
-            // Étape 6 : Envoyer un message à la file
-            MessageProducer producer = session.createProducer(queue);
+            producer.send(message);
             System.out.println("Envoi du message : '" + payload + "'");
-            producer.send(message); // Envoi du message
 
-            // Étape 7 : Créer un consommateur pour lire les messages
+            // Consommation du message
             MessageConsumer consumer = session.createConsumer(queue);
-
-            // Étape 8 : Démarrer la connexion pour activer la réception des messages
             connection.start();
-
-            // Étape 9 : Recevoir le message de la file
-            TextMessage receivedMessage = (TextMessage) consumer.receive(); // Lecture synchrone
+            TextMessage receivedMessage = (TextMessage) consumer.receive();
             System.out.println("Message reçu : " + receivedMessage.getText());
 
-            // Fermer la session
-            session.close();
         } finally {
-            if (connection != null) {
-                connection.close(); // Fermer la connexion
-            }
-            broker.stop(); // Arrêter le broker
+            if (session != null) session.close();
+            if (connection != null) connection.close();
         }
     }
 }
--- src/main/java/CyrilEnzoI/TP1_ASI/Tp1AsiApplication.java
+++ src/main/java/CyrilEnzoI/TP1_ASI/Tp1AsiApplication.java
@@ -1,56 +1,30 @@
 package CyrilEnzoI.TP1_ASI;
 
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Scanner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Tp1AsiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1AsiApplication.class, args);
+	}
 
-		Scanner scanner = new Scanner(System.in);
-		boolean continuer = true;
+	@Bean
+	public BrokerService brokerService() throws Exception {
+		BrokerService broker = new BrokerService();
+		broker.addConnector("tcp://localhost:61616");
+		broker.start();
+		return broker;
+	}
 
-		while (continuer) {
-			// Demande à l'utilisateur quel exercice lancer
-			System.out.println("\nChoisissez l'exercice à exécuter :\n1. JMS Queue\n2. JMS Topic\n==>");
-			int choix = scanner.nextInt();
-
-			switch (choix) {
-				case 1:
-					System.out.println("Lancement de l'exercice 1 : JMS Queue");
-					try {
-						JmsQueueExample.main(args);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					break;
-				case 2:
-					System.out.println("Lancement de l'exercice 2 : JMS Topic");
-					try {
-						JmsTopicExample.main(args);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					break;
-				default:
-					System.out.println("Choix invalide. Veuillez entrer 1 ou 2.");
-					continue;
-			}
-
-			// Demande si l'utilisateur veut exécuter un autre exercice
-			System.out.println("\nVoulez-vous exécuter un autre exercice ? (oui/non)\n==>");
-			String reponse = scanner.next();
-			continuer = reponse.equalsIgnoreCase("oui");
-
-			if (!continuer) {
-				System.out.println("Merci d'avoir utilisé le programme. Au revoir !");
-			}
-		}
-
-		scanner.close();
+	@Bean
+	CommandLineRunner start(JmsQueueExample jmsQueueExample) {
+		return args -> {
+			jmsQueueExample.processQueue(); // Lancer le traitement JMS proprement
+		};
 	}
 }
